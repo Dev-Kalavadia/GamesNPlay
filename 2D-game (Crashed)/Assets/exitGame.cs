95040c5cec8774204fc7937e34dec1763c66d1f4
Dev Kalavadiya <67102702+Dev-Kalavadia@users.noreply.github.com>
2023-03-31 19:11:50 +0400

    Uploading 2d game files

---
+++ 2D-game (Crashed)/Assets/exitGame.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitGame : MonoBehaviour
+{
+    public void Exit()
+    {
+       // Debug.Log("Exiting the game...");
+    	Application.Quit();
+    }
+}
